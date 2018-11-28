5ca838ddc4b691ea98157ef3313ac2d292462d04
U-DESKTOP-D9BMJNT\zejri <zejrik5@yahoo.se>
2018-11-28 13:25:36 +0100

    Ändrat Position i Player till enums ist för String och fixat testklasserna

--- projekt/src/main/resources/Player.java
+++ projekt/src/main/resources/Player.java
@@ -4,14 +4,26 @@ import src.main.resources.Card;
 
 public class Player {
 
+  public enum Position {
+    SB,
+    BB,
+    UTG,
+    UTG1,
+    UTG2,
+    MP,
+    LJ,
+    HJ,
+    CO,
+    BUT;
+  }
+
   private final String name;
   private final int stack;
-  private final String position;
+  private Position position;
   private final Card card1;
   private final Card card2;
 
-
-  public Player(String name, int stack, String position, Card card1, Card card2){
+  public Player(String name, int stack, Position position, Card card1, Card card2){
     this.name = name;
     this.stack = stack;
     this.position = position;
@@ -21,11 +33,11 @@ public class Player {
 
   public String name() { return name; }
   public int stack() { return stack; }
-  public String position() { return position; }
+  public Position position() { return position; }
   public Card card1() { return card1; }
   public Card card2() { return card2; }
 
-  public Player(String name, int stack, String position){
+  public Player(String name, int stack, Position position){
     this.name = name;
     this.stack = stack;
     this.position = position;
