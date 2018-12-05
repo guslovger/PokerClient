2f449867d481ab4b63a7aa9792f2b3e0acabfb71
guslucaka <guslucaka@student.gu.se>
2018-12-05 15:19:55 +0100

    Lagt till addPlayer i databasen

--- projekt/src/test/resources/TestDatabase.java
+++ projekt/src/test/resources/TestDatabase.java
@@ -7,30 +7,74 @@ public class TestDatabase {
    /**
    * Connect to a sample database
    */
-  public static void connect() {
-      Connection conn = null;
-      try {
-          // db parameters
-          String url = "jdbc:sqlite:lib/PokerDatabase.db";
-          // create a connection to the database
-          conn = DriverManager.getConnection(url);
+   public static void connect() {
+     Connection conn = null;
+     try {
+       // db parameters
+       String url = "jdbc:sqlite:lib/PokerDatabase.db";
+       // create a connection to the database
+       conn = DriverManager.getConnection(url);
+       System.out.println("Connection to SQLite has been established.");
 
-          System.out.println("Connection to SQLite has been established.");
+       String query = "SELECT * FROM players";
+       Statement st = conn.createStatement();
+       ResultSet rs = st.executeQuery(query);
 
-      } catch (SQLException e) {
-          System.out.println(e.getMessage());
-      } finally {
-          try {
-              if (conn != null) {
-                  conn.close();
-              }
-          } catch (SQLException ex) {
-              System.out.println(ex.getMessage());
-          }
+       while (rs.next()) {
+         int id = rs.getInt("id");
+         String name = rs.getString("Player_NAME");
+         System.out.println(id + " | " + name);
+       }
+       st.close();
+     } catch (SQLException e) {
+       System.out.println(e.getMessage());
+     } finally {
+       try {
+         if (conn != null) {
+           conn.close();
+         }
+       } catch (SQLException ex) {
+         System.out.println(ex.getMessage());
+       }
+     }
+   }
+
+  public static void printPlayers() {
+    try (Connection conn = connect2()){
+      System.out.println("Sucess");
+      String query = "SELECT * FROM players";
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery(query);
+
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        String name = rs.getString("Player_NAME");
+        System.out.println(id + " | " + name);
       }
+      st.close();
+    } catch (SQLException e){
+      System.err.println("SQL problem: " + e.getMessage());
+    }
   }
 
-  private Connection connect2() {
+  public static void addPlayer (String name){
+    String sql = "INSERT INTO players(Player_NAME) VALUES (?)";
+    //String lastPlayerID = "SELECT Player_ID FROM players WHERE Player_ID = Max(Player_ID)";
+    try (Connection conn = connect2()){
+      //Statement st = conn.createStatement();
+      PreparedStatement ps = conn.prepareStatement(sql);
+      ps.setString(1, name);
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      System.err.println("Problem: " + e.getMessage());
+    }
+  }
+
+
+
+
+
+  private static Connection connect2() {
         // SQLite connection string
         String url = "jdbc:sqlite:lib/PokerDatabase.db";
         Connection conn = null;
@@ -41,7 +85,7 @@ public class TestDatabase {
         }
         return conn;
     }
-
+/*
   public void addPlayer(int id, String name){
     String sql = "INSERT INTO player(ID,NAME) VALUES(?,?)";
 
@@ -54,12 +98,15 @@ public class TestDatabase {
             System.out.println(e.getMessage());
         }
 
-  }
+  }/*
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-      connect();
+      printPlayers();
+      String kalle = "Kalle";
+      addPlayer(kalle);
+      printPlayers();
       //TestDatabase test = new TestDatabase();
       //test.addPlayer(1, "Lucas");
       //test.addPlayer(2, "Erik");
