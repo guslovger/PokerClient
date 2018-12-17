de34e596855e01995f040d6798d05f72c9adfc0f
guslovger <zejrik5@yahoo.se>
2018-12-17 14:11:50 +0100

    ändrat PokerHand

--- projekt/src/main/java/PokerHand.java
+++ projekt/src/main/java/PokerHand.java
@@ -8,23 +8,12 @@ import src.main.resources.Card;
 
 public class PokerHand {
 
-  public String hand;
-  public ArrayList<Player> players;
-  public int potSize;
-
-  public PokerHand(String hand){
-    this.hand = hand;
-    this.players = new ArrayList<>();
-  }
-  public PokerHand(String hand, ArrayList<Player> players){
-    this.hand = hand;
-    this.players = players;
-  }
-  public String hand(){
-   return hand;
-  }
-  public ArrayList<Player> players(){
-    return players;
+  List<Player> players;
+  List<Street> streets;
+  void run() {
+    for (Street st : streets) {
+      st.run();
+    }
   }
 
 }
---
+++ projekt/src/main/resources/StreetAction.java
@@ -0,0 +1,30 @@
+package src.main.resources;
+
+public class StreetAction {
+
+  Street street;
+
+  enum Type {
+    BET,
+    FOLD,
+    CALL,
+    RAISE,
+    CHECK,
+    ALLIN;
+  }
+
+  Player p;
+  double amount;
+  Action.Type type;
+
+  Player player() { return player; }
+  double amount() { return amount; }
+  Action.Type type() { return type; }
+
+  void do() {
+    for(Action action : actions)
+  }
+}
+
+
+}
